a19973e2d0888e251c2bdc4e7b89def794017543
xiaoxihurong <1120407159@qq.com>
2019-10-03 22:09:42 +0800

    第二天完成

--- src/components/tabbar/HomeContainer.vue
+++ src/components/tabbar/HomeContainer.vue
@@ -2,52 +2,65 @@
     <div>
         <!-- 轮播图区域 -->
         <mt-swipe :auto="4000">
-            <mt-swipe-item >
+            <mt-swipe-item>
                 <img src="https://img.zcool.cn/community/01ac855a4b5445a801206ed3eef8ea.jpg@2o.jpg">
             </mt-swipe-item>
-             <mt-swipe-item >
+            <mt-swipe-item>
                 <img src="https://img.zcool.cn/community/017f675a4b5445a801206ed3eab420.jpg@1280w_1l_2o_100sh.jpg">
             </mt-swipe-item>
-             <mt-swipe-item >
+            <mt-swipe-item>
                 <img src="https://img.zcool.cn/community/0197935a4b5443a801206ed3be47e3.jpg@1280w_1l_2o_100sh.jpg">
             </mt-swipe-item>
-             <mt-swipe-item >
+            <mt-swipe-item>
                 <img src="https://img.zcool.cn/community/01ea8b5a4b53dfa80121974131ce05.jpg@1280w_1l_2o_100sh.jpg">
             </mt-swipe-item>
-             <mt-swipe-item >
+            <mt-swipe-item>
                 <img src="https://img.zcool.cn/community/01c9155a4b53dfa801206ed3aebbd7.jpg@1280w_1l_2o_100sh.jpg">
             </mt-swipe-item>
         </mt-swipe>
-
-         <!-- 九宫格 到 6宫格 的改造工程 -->
+        <!-- 九宫格 到 6宫格 的改造工程 -->
         <ul class="mui-table-view mui-grid-view mui-grid-9">
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/home/newslist">
                     <img src="../../images/menu1.png" alt="">
-                    <div class="mui-media-body">新闻资讯</div></a></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                    <div class="mui-media-body">新闻资讯</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
                     <img src="../../images/menu2.png" alt="">
-                    <div class="mui-media-body">图片分享</div></a></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                    <div class="mui-media-body">图片分享</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
                     <img src="../../images/menu3.png" alt="">
-                    <div class="mui-media-body">商品购买</div></a></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                    <div class="mui-media-body">商品购买</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
                     <img src="../../images/menu4.png" alt="">
-                    <div class="mui-media-body">留言反馈</div></a></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                    <div class="mui-media-body">留言反馈</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
                     <img src="../../images/menu5.png" alt="">
-                    <div class="mui-media-body">视频专区</div></a></li>
-            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                    <div class="mui-media-body">视频专区</div>
+                </router-link>
+            </li>
+            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                <router-link to="/">
                     <img src="../../images/menu6.png" alt="">
-                    <div class="mui-media-body">联系我们</div></a></li>
-        </ul> 
-    
+                    <div class="mui-media-body">联系我们</div>
+                </router-link>
+            </li>
+        </ul>
     </div>
 </template>
-
 <script type="text/javascript">
-    
 </script>
-
 <style type="text/css">
 .mint-swipe {
     height: 200px;
@@ -59,8 +72,8 @@
 }
 
 .mui-grid-view.mui-grid-9 {
-  background-color: #fff;
-  border: none;
+    background-color: #fff;
+    border: none;
 }
 
 .mui-grid-view.mui-grid-9 img {
@@ -68,12 +81,12 @@
     height: 60px;
 }
 
- .mui-grid-view.mui-grid-9 .mui-media-body{
+.mui-grid-view.mui-grid-9 .mui-media-body {
     font-size: 13px;
 }
 
 .mui-grid-view.mui-grid-9 .mui-table-view-cell {
-  border: 0;
+    border: 0;
 }
-
 </style>
+
--- src/main.js
+++ src/main.js
@@ -11,17 +11,25 @@ Vue.use(VueRouter)
 import VueResource from 'vue-resource'
 // 2.2 安装 vue-resource
 Vue.use(VueResource)
+Vue.http.options.root = 'http://www.liulongbin.top:3005';
 
+// 导入格式化时间的插件
+import moment from 'moment'
+// 定义全局的过滤器
+Vue.filter('dateFormat', function (dataStr, pattern = "MM-DD HH:mm") {
+  return moment(dataStr).format(pattern)
+})
 
 import './lib/mui/css/mui.min.css'  // 导入 MUI 的样式
 
 import './lib/mui/css/icons-extra.css' // 导入扩展图标样式
 
 // 按需导入 Mint-UI 中的组件 
-import { Header, Swipe, SwipeItem } from 'mint-ui'
+import { Header, Swipe, SwipeItem ,Button} from 'mint-ui'
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe)
 Vue.component(SwipeItem.name, SwipeItem)
+Vue.component(Button.name, Button)
 
 
 import app  from './App.vue'      // 导入 App 根组件
