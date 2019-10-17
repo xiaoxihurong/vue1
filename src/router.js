ff388ad13212fb9f443a5981fd216f39835b5f5e
xiaoxihurong <56077154+xiaoxihurong@users.noreply.github.com>
2019-10-17 14:42:30 +0800

    Update router.js

--- src/router.js
+++ src/router.js
@@ -2,9 +2,7 @@ import VueRouter from 'vue-router'
 
 // 导入对应的路由组件
 import HomeContainer from './components/tabbar/HomeContainer.vue'
-import MemberContainer from './components/tabbar/MemberContainer.vue'
 import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
-import SearchContainer from './components/tabbar/SearchContainer.vue'
 import NewsList from './components/news/NewsList.vue'
 import NewsInfo from './components/news/NewsInfo.vue'
 import PhotoList from './components/photos/PhotoList.vue'
@@ -18,9 +16,7 @@ var router = new VueRouter({
   routes: [ // 配置路由规则
     { path: '/', redirect: '/home' },
     { path: '/home', component: HomeContainer},
-    { path: '/member', component: MemberContainer },
     { path: '/shopcar', component: ShopcarContainer },
-    { path: '/search', component: SearchContainer },
     { path: '/home/newslist',component:NewsList},
     { path: '/home/newsinfo/:id',component:NewsInfo},
     { path: '/home/photolist',component:PhotoList},
